6f08005611518be433869d12f08787d8ebff8698
matt4567 <matt.moore35@yahoo.co.uk>
2017-08-07 15:50:40 +0100

    Added multiple datasets to graph at once
    I've added a change inside of updataChartData() method so when you add the chart dataset and ngOnChanges() is called the data you have added it is added on top of existing dataset. This allows you to add multiple datasets dynamically without writing over your previous datasets.

--- src/charts/charts.ts
+++ src/charts/charts.ts
@@ -126,7 +126,7 @@ export class BaseChartDirective implements OnDestroy, OnChanges, OnInit {
   private updateChartData(newDataValues: number[] | any[]): void {
     if (Array.isArray(newDataValues[0].data)) {
       this.chart.data.datasets.forEach((dataset: any, i: number) => {
-        dataset.data = newDataValues[i].data;
+        dataset.data = dataset.data + newDataValues[i].data;
 
         if (newDataValues[i].label) {
           dataset.label = newDataValues[i].label;
